433a6ceb3c3bec75875a0a3224055b743f5ac832
Irvan Sandoval <irvan.sandoval@gmail.com>
2023-02-21 00:06:18 +0700

    LAST

---
+++ application/logs/log-2023-02-21.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-02-21 00:02:58 --> Could not find the language line "Purchase Quotation Added QT-000001"
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: Too many login failures (errflg=2) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: [AUTHENTICATIONFAILED] Authentication failed. (errflg=1) Unknown 0
+ERROR - 2023-02-21 00:03:41 --> Severity: Notice --> Unknown: Too many login failures (errflg=2) Unknown 0
